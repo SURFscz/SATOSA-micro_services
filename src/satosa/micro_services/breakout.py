823c9d4e89b052538f6d1b8834fe32bbaddfd8d8
Martin van Es <martin@mrvanes.com>
2018-09-03 15:24:02 +0200

    Add comments to clarify inner breakout workings

--- src/satosa/micro_services/breakout.py
+++ src/satosa/micro_services/breakout.py
@@ -20,6 +20,9 @@ class BreakOut(ResponseMicroService):
     """
 
     def __init__(self, config, internal_attributes, *args, **kwargs):
+        """
+        Initialise breakout micro service
+        """
         super().__init__(*args, **kwargs)
         self.name = "breakout"
         self.endpoint = "/resume"
@@ -29,13 +32,17 @@ class BreakOut(ResponseMicroService):
 
     def register_endpoints(self):
         """
-        Register breakout module endpoints
+        Register breakout module (resume) endpoint on initialisation
         :return: A list of endpoints bound to a function
         """
         logger.info("Register BreakOut endpoint")
         return [("^breakout%s$" % self.endpoint, self._handle_endpoint)]
 
     def process(self, context, internal_response):
+        """
+        This is the main workhorse. It's main duty is storing the internal_resonse
+        so we can pick it up when we resume
+        """
         logger.info("Process BreakOut")
         context.state[STATE_KEY] = {}
         context.state[STATE_KEY]["internal_resp"] = internal_response.to_dict()
@@ -43,15 +50,24 @@ class BreakOut(ResponseMicroService):
         return self._check_requirement(context, internal_response)
 
     def _check_requirement(self, context, internal_response):
+        """
+        Assume BreakOut is only needed under certain conditions
+        This is an example method that checks condition
+        """
         logger.info("Check BreakOut requirement")
         if self.resumed:
+            # If everything is ok, don't break out and continue
             return super().process(context, internal_response)
         else:
+            # This is actual breakout redirect
             return Redirect(self.redirect_url)
 
     def _handle_endpoint(self, context):
+        """
+        The resume endpoint handler. It's main duty is restoring internal_response
+        and checking the resume condition
+        """
         logger.info("Handle BreakOut endpoint")
-
         breakout_state = context.state[STATE_KEY]
         saved_response = breakout_state["internal_resp"]
         logger.info("internal_resp: %s" % saved_response)
