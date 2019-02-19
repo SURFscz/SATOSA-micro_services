6f00201c4edab5020b20f816636f405e4f0918d8
Okke Harsta <oharsta@zilverline.com>
2019-02-19 13:55:49 +0100

    Formatting

---
+++ src/test/test_custom_uid.py
@@ -0,0 +1,30 @@
+import importlib
+from unittest import TestCase
+from uuid import uuid4
+
+from munch import munchify
+from satosa.logging_util import LOGGER_STATE_KEY
+
+custom_uid = importlib.import_module("src.scz-micro_services.custom_uid")
+
+
+class TestCustomUid(TestCase):
+
+    def test_custom_uid(self):
+        context = munchify({"state": {LOGGER_STATE_KEY: uuid4().urn, "IDHASHER": {}},
+                            "select": {"a": "b", "c": "d"},
+                            "custom_attribute": "",
+                            "user_id": "urn:john"})
+        c_uid = custom_uid.CustomUID(context, name="custom_uid", base_url="http://localhost")
+
+        def next_call(ctx={}, data={}):
+            pass
+
+        c_uid.__dict__["next"] = next_call
+
+        self.assertEqual(c_uid.logprefix, "CUSTOM_UID:")
+
+        data = munchify({"name_id": "urn:john", "attributes": {"a": "b1", "c": "d1"}})
+        c_uid.process(context, data)
+
+        self.assertEqual("b|1|d|1", data.user_id)
